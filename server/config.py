039bc33f32ea1eb1a21ed3dfd0d624294a469f42
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2022-09-25 23:04:02 +0000

    initial commit, setup environments, configured variables

---
+++ server/config.py
@@ -0,0 +1,23 @@
+
+from decouple import config
+
+
+DB_NAME=config('DB_NAME')
+DB_USERNAME=config('DB_USERNAME')
+DB_USER_PASS=config('DB_USER_PASS')
+DB_CONNECTION_PORT=config('DB_CONNECTION_PORT')
+
+class Config:
+    SECRET_KEY=config('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS=config('SQLALCHEMY_TRACK_MODIFICATIONS', cast=bool)
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI=f'postgresql://{DB_USERNAME}:{DB_USER_PASS}@{DB_CONNECTION_PORT}/{DB_NAME}'
+    DEBUG=config('DEBUG', cast=bool)
+    SQLALCHEMY_ECHO=config('SQLALCHEMY_ECHO', cast=bool)
+
+class ProConfig(Config):
+    pass
+
+class TestConfig(Config):
+    pass

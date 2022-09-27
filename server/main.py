a5c2a1f8b7e67b6fff15d4e1a1eea5a3b587b3c3
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2022-09-27 00:26:21 +0000

    created all routes endpoints for user model and post model, tested all rout end points

--- server/main.py
+++ server/main.py
@@ -1,26 +1,145 @@
-from flask import Flask
-from flask_restx import (Api, Resource)
+from turtle import title
+from flask import Flask, request
+from flask_restx import Api, Resource, fields
 from config import DevConfig
-from models import (User, Post)
+from models import User, Post
 from exts import db
+from sqlalchemy import asc, desc
 
-app=Flask(__name__)
+app = Flask(__name__)
 app.config.from_object(DevConfig)
 db.init_app(app)
-api=Api(app, doc='/docs')
+api = Api(app, doc="/docs")
+users_model = api.model(
+    "Users",
+    {
+        "id": fields.String(),
+        "username": fields.String(),
+        "user_email": fields.String(),
+        "avatar": fields.String(),
+    },
+)
 
-@api.route('/hello')
+posts_model = api.model(
+    "Posts",
+    {
+        "id": fields.String(),
+        "title": fields.String(),
+        "date_posted": fields.String(),
+        "post_content": fields.String(),
+        "user_id": fields.String(),
+    },
+)
+
+
+@api.route("/hello")
 class HelloResource(Resource):
     def get(self):
-        return {"msg":"Active!"}
+        return {"msg": "Active!"}
+
+
+@api.route("/users")
+class UsersResource(Resource):
+    @api.marshal_list_with(users_model)
+    def get(self):
+        """get all users from database"""
+        users = User.query.order_by(User.username).all()
+        return users, 200
+
+    @api.marshal_with(users_model)
+    def post(self):
+        """create a new user"""
+        data = request.get_json()
+        new_user = User(
+            username=data.get("username"),
+            user_email=data.get("user_email"),
+            password=data.get("password"),
+            avatar=data.get("avatar"),
+        )
+        new_user.insert()
+        return new_user, 201
+
+
+@api.route("/user/<id>")
+class UsersResource(Resource):
+    @api.marshal_with(users_model)
+    def get(self):
+        """get specific user from database"""
+        user = User.query.get_or_404(id)
+        return user, 200
+
+    @api.marshal_with(users_model)
+    def put(self, id):
+        """update user details in database"""
+        data = request.get_json()
+        user_to_be_updated = User.query.get_or_404(id)
+        user_to_be_updated.update(
+            data.get("username"),
+            data.get("user_email"),
+            data.get("password"),
+            data.get("avatar"),
+        )
+        return user_to_be_updated, 200
+
+    @api.marshal_with(users_model)
+    def delete(self, id):
+        """delete a user from the database"""
+        user_to_be_deleted = User.query.get_or_404(id)
+        user_to_be_deleted.delete()
+        return user_to_be_deleted, 200
+
+
+@api.route("/posts")
+class PostsResource(Resource):
+    @api.marshal_list_with(posts_model)
+    def get(self):
+        """get all posts from database"""
+        posts = Post.query.order_by(Post.title).all()
+        return posts, 200
+
+    @api.marshal_with(posts_model)
+    def post(self):
+        """create a new post"""
+        data = request.get_json()
+        new_post = Post(
+            title=data.get("title"),
+            post_content=data.get("post_content"),
+            user_id=data.get("user_id"),
+        )
+        new_post.insert()
+        return new_post, 201
+
+
+@api.route("/post/<id>")
+class PostsResource(Resource):
+    @api.marshal_with(posts_model)
+    def get(self, id):
+        """get a specific post"""
+        print(type(id))
+        single_post = Post.query.get_or_404(id)
+        return single_post
+
+    @api.marshal_with(posts_model)
+    def put(self, id):
+        """update a specific post"""
+        data = request.get_json()
+        post_to_update = Post.query.get_or_404(id)
+        post_to_update.update(data.get("title"), data.get("post_content"))
+        return post_to_update, 200
+
+    @api.marshal_with(posts_model)
+    def delete(self, id):
+        """delete a specific post"""
+        post_to_delete = Post.query.get_or_404(id)
+        post_to_delete.delete()
+
+        return post_to_delete, 200
+
 
 @app.shell_context_processor
 def make_shell_context():
-    return {
-        "db": db,
-        "User": User,
-        "Post": Post
-    }
+    return {"db": db, "User": User, "Post": Post}
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
--- server/models.py
+++ server/models.py
@@ -2,15 +2,13 @@ from datetime import datetime
 from exts import db
 from uuid import uuid4
 import shortuuid
-import json
-from typing import Dict
+from sqlalchemy.dialects.postgresql import UUID
 
-def gen_user_id():
-    return uuid4()
+
 def gen_post_id():
     return shortuuid.uuid()
-def gen_date():
-    return datetime.utcnow()
+
+
 """
 class Users:
     id: String primary_key=True unique=True default=gen_uuid
@@ -19,21 +17,16 @@ class Users:
     password: String(60) nullable=False
     avatar: String(120) nullable=True default='default.jpg'
 """
-class User(db.Model):
-    id=db.Column(db.String(36), primary_key=True, unique=True, default=gen_user_id)
-    username=db.Column(db.String(20), unique=True, nullable=False)
-    user_email=db.Column(db.String(120), unique=True, nullable=False)
-    password=db.Column(db.String(60), nullable=False)
-    avatar=db.Column(db.String(30), nullable=False, default='default.jpg')
-    posts=db.relationship('Post', backref='author', lazy=True)
 
-    def display(self) -> Dict:
-        return {
-            'id': self.id,
-            'username': self.username,
-            'user_email': self.user_email,
-            'avatar': self.avatar
-        }
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(UUID(as_uuid=True), primary_key=True, unique=True, default=uuid4)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    user_email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    avatar = db.Column(db.String(30), nullable=False, default="default.jpg")
+    posts = db.relationship("Post", backref="author", lazy=True)
 
     def insert(self) -> None:
         db.session.add(self)
@@ -44,14 +37,18 @@ class User(db.Model):
         db.session.commit()
 
     def update(self, username, user_email, password, avatar) -> None:
-        self.username=username
-        self.user_email=user_email
-        self.password=password
-        self.avatar=avatar
+        self.username = username
+        self.user_email = user_email
+        self.password = password
+        self.avatar = avatar
         db.session.commit()
 
     def __repr__(self):
-        return json.dumps(self.display())
+        return (
+            f"User('{self.id}', '{self.username}', '{self.password}', '{self.avatar}')"
+        )
+
+
 """
 class Post:
     id: String primary_key=True unique=True default=gen_uuid
@@ -59,20 +56,16 @@ class Post:
     date_posted: DateTime nullable=False default=gen_date
     post_content: Text nullable=False
 """
-class Post(db.Model):
-    id=db.Column(db.String(22), primary_key=True, unique=True, default=gen_post_id)
-    title=db.Column(db.String(100), nullable=False)
-    date_posted=db.Column(db.DateTime, nullable=False, default=gen_date)
-    post_content=db.Column(db.Text, nullable=False)
-    user_id=db.Column(db.String(36), db.ForeignKey('user.id'), nullable=False)
 
-    def display(self) -> Dict:
-        return {
-            'id': self.id,
-            'title': self.title,
-            'date_posted': self.date_posted,
-            'post_content': self.post_content
-        }
+
+class Post(db.Model):
+    __tablename__ = "posts"
+    id = db.Column(db.String(22), primary_key=True, unique=True, default=gen_post_id)
+
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    post_content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey("users.id"), nullable=False)
 
     def insert(self) -> None:
         db.session.add(self)
@@ -82,12 +75,10 @@ class Post(db.Model):
         db.session.delete(self)
         db.session.commit()
 
-    def update(self, title, date_posted, post_content, avatar) -> None:
-        self.title=title
-        self.date_posted=date_posted
-        self.post_content=post_content
-        self.avatar=avatar
+    def update(self, title, post_content) -> None:
+        self.title = title
+        self.post_content = post_content
         db.session.commit()
-    
+
     def __repr__(self):
-        return json.dumps(self.display())
+        return f"Post('{self.id}', '{self.title}', '{self.date_posted}', '{self.post_content}')"

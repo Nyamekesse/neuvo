dd7069a58f30541295b7f93eaa66e530d2f218d5
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-03-16 04:43:32 +0000

    updated editor section

--- client/src/components/Editor/index.jsx
+++ client/src/components/Editor/index.jsx
@@ -1,61 +1,11 @@
 import React from "react";
-import { EditorState } from "draft-js";
-import { Editor } from "react-draft-wysiwyg";
-import { convertToHTML } from "draft-convert";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import { useState } from "react";
-import "./style.css";
-import { useEffect } from "react";
-import DOMPurify from "dompurify";
+import MainEditor from "./MainEditor";
 
-function createMarkup(html) {
-  return {
-    __html: DOMPurify.sanitize(html),
-  };
-}
 const CustomEditor = () => {
-  const [editorState, setEditorState] = useState(() =>
-    EditorState.createEmpty()
-  );
-  const [convertedContent, setConvertedContent] = useState(null);
-
-  useEffect(() => {
-    let html = convertToHTML(editorState.getCurrentContent());
-
-    setConvertedContent(html);
-  }, [editorState]);
   return (
     <>
-      <div style={{ width: "100%" }}>
-        <Editor
-          editorState={editorState}
-          onEditorStateChange={setEditorState}
-          wrapperClassName="wrapper-class"
-          editorClassName="editor-class"
-          toolbarClassName="toolbar-class"
-          toolbar={{
-            options: ["inline", "blockType", "fontFamily"],
-            fontFamily: {
-              options: [
-                "Arial",
-                "Georgia",
-                "Impact",
-                "Tahoma",
-                "Times New Roman",
-                "Verdana",
-              ],
-            },
-          }}
-        />
-      </div>
-      <div>
-        <div
-          className="preview"
-          dangerouslySetInnerHTML={createMarkup(convertedContent)}
-        ></div>
-      </div>
+      <MainEditor />
     </>
   );
 };
-
 export default CustomEditor;
